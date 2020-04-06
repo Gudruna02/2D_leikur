3d10bbc947bb9a4b4472fac2761997314163fde0
Guðrún Ásta <42605676+Gudruna02@users.noreply.github.com>
2020-04-06 03:22:20 +0000

    Add files via upload

---
+++ Scripts/PlayerControler.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Collider2D coll;
+
+    private enum State {idle, running, jumping, falling, hurt}
+    private State state = State.idle;
+    
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private int cherries = 0;
+    [SerializeField] private Text cherryText;
+    [SerializeField] private float hurtForce = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if(state != State.hurt)
+        {
+            Movement();
+        }
+        Movement();
+        AnimationState();
+        anim.SetInteger("state", (int)state);  
+    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Collectable")
+        {
+            Destroy(collision.gameObject);
+            cherries += 1;
+            cherryText.text = cherries.ToString();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            if(state == State.falling)
+            {
+                Destroy(other.gameObject);
+            }
+            
+            else
+            {
+                state = State.hurt;
+                if(other.gameObject.transform.position.x > transform.position.x)
+                {
+                    //Óvinur til hægri þá fer ég til vinstri
+                    rb.velocity = new Vector2(-hurtForce, rb.velocity.y);
+                }
+                else
+                {
+                    //Óvinur til vinstri þa fer eg til hægri
+                    rb.velocity = new Vector2(hurtForce, rb.velocity.y);
+                }
+            }
+        }
+    }
+
+    private void Movement()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+
+        //Vinstri
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-speed,  rb.velocity.y);
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+        //Hægri
+        else if(hDirection > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+            transform.localScale = new Vector2(1, 1);
+
+        }
+   
+        //Hoppa
+        if(Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            state = State.jumping;
+        }
+    }
+
+
+
+    private void AnimationState()
+    {
+
+
+
+        if(state == State.jumping)
+        {
+            if(rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+        else if(state == State.falling)
+        {
+            if(coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+        else if (state == State.hurt)
+        {
+            if (Mathf.Abs(rb.velocity.x) < .1f)
+            {
+                state = State.idle;
+            }
+        }
+
+
+        else if(Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Hreyfing
+            state = State.running;
+        }
+
+        else
+        {
+            state = State.idle;
+        }
+    }
+}
